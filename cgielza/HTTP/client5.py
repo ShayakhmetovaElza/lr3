aa22f1b326d0bd88dce1573216df0323ce8b133d
itsnast <63636485+itsnast@users.noreply.github.com>
2020-12-28 14:49:13 +0500

    project

---
+++ cgielza/HTTP/client5.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+def DoRequest(method, cmd="", data=""):
+	try:
+		url = 'http://localhost:8080/cgi-bin/APIClient.py'
+		header = {"Content-type": 'application-json'}
+		res = method(url+cmd, headers=header, data=json.dumps(data))
+		if res.status_code == 200:
+			print(res.content.decode())
+			return json.loads(res.content)
+	except Exception as ex:
+		print(ex)
+
+
+def GetList():
+	res = DoRequest(requests.get)
+	print(res)
+	for i, title in res['ids']:
+		print(f"{i}: {title}")
+		
+		
+		
+		
+GetList()
